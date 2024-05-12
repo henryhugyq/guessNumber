a399898df48e16a9caee45449a45638ba8bc39eb
henryhugyq <89083615+henryhugyq@users.noreply.github.com>
2024-05-12 15:59:35 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,34 @@
 import { useState, useEffect } from 'react'
 import {Contract, ethers, } from 'ethers';
 import Head from "next/head";
+import {
+    Layout,
+    Nav,
+    Button,
+    Breadcrumb,
+    Skeleton,
+    Avatar,
+    Dropdown,
+    Empty,
+    Popover,
+    Tooltip,
+    Toast,
+    Notification
+} from '@douyinfe/semi-ui';
+import {
+    IconSemiLogo,
+    IconBell,
+    IconHelpCircle,
+    IconBytedanceLogo,
+    IconHome,
+    IconLive,
+    IconSetting,
+    IconLock, IconGlobe, IconGallery, IconUnlock, IconSun, IconMoon, IconPuzzle, IconBeaker
+} from '@douyinfe/semi-icons';
+import ContractDeploy from "./ContractDeploy";
+import ContractCall from "./ContractCall";
+import Setting from "./Setting";
+
 
 function Home() {
     //ethers v6
@@ -9,25 +37,14 @@ function Home() {
     const [chainId,setChainId] = useState();
     const [contract, setContract] = useState();
     const [provider,setProvider] = useState();
-    const [to,setTo] = useState();
+    const [contractAddress, setContractAddress] = useState(null);
     const[amount,setAmount] = useState();
-    const[greet,setGreeting] = useState();
-
-    //guessNumber
-    const[number,setNumber] = useState();
+    const [switchStatus,setswitchStatus] = useState(true)
+    const [selectedKey, setSelectedKey] = useState('Home');
     // let constract = new Contract("",abi,singer)
 
-    //合约地址和可读abi
-    const MYTOKEN_ADDRESS = "0xab86B52ae30bf04bF0F5Cb843cCb0060582C7aDb";
-    const MYTOKEN_ABI = [
-        "function owner() public view returns (address)",
-        "function guessNumber(uint _guess) external payable",
-        "function withdraw()",
-        "function gameEnded() public view returns (bool)",
-        "function endGame() external",
-        "function restartGame(uint _newTargetNumber) external",
-        "function setTargetNumber(uint _newTargetNumber) external",
-    ];
+    const { Header, Footer, Content } = Layout;
+
 
     //点击按钮的时候登录
     const connectOnclick = async() => {
@@ -49,8 +66,6 @@ function Home() {
         //获取signer来创建contract实例
         const signer = await providerWeb3.getSigner();
 
-        const contract = await new Contract(MYTOKEN_ADDRESS,MYTOKEN_ABI,signer);
-        setContract(contract)
         //获取余额
         const currentBalance = await providerWeb3.getBalance(currenAccount[0]);
         setBalance(ethers.formatEther(currentBalance));
@@ -62,91 +77,55 @@ function Home() {
                 setBalance(ethers.formatEther(result))
             });
         })
-        //获取chainId
-        const chainId = await window.ethereum.request({method:"eth_chainId"})
-        window.ethereum.on("chainChanged", handleChainChanged);
-        setChainId(chainId)
+        Notification.success({
+            title: 'Connected to Wallet',
+            content: 'You have successfully connected to your wallet.',
+            duration: 3,
+            position: 'topLeft'
+        });
 
     }
+    const goToContractCall = (address) => {
+        setSelectedKey('ContractCall'); // 更新 selectedKey
+        setContractAddress(address); // 存储合约地址
+    };
+    const handleMenuClick = (itemKey) => {
+        setSelectedKey(itemKey);
+
+    };
+    const ToastBalance = async ()=>{
+        let opts = {
+            content: 'Hi,Your Balance is<'+ balance + ">ETH",
+            duration: 3,
+            theme:"light"
+        }
+        Toast.info(opts)
+    }
+    //显示账户
+    const ToastAccount = async ()=>{
+        let opts = {
+            content: 'Hi,Your Account is<'+ account + ">",
+            duration: 3,
+            theme:"light"
+        }
+        Toast.info(opts)
+    }
+
+    const switchMode = () => {
+        const body = document.body;
+        if (body.hasAttribute('theme-mode')) {
+            body.removeAttribute('theme-mode');
+
+            setswitchStatus(true)
+        } else {
+            body.setAttribute('theme-mode', 'dark');
+            setswitchStatus(false)
+
+        }
+    };
 
 
 
-    const getOwner = async() =>{
-        console.log(contract)
-        try{ let a  = await contract.owner();
-            alert(a);
-        }catch (error){
-            alert(error)
-        }
-        }
-        //guessNumber
-    const guessNumber = async ()=>{
-        try{
-            const tx = await contract.guessNumber(number,{
-                value: ethers.parseEther('0.01')
-            })
-            await tx.wait()
-            alert(tx.hash)
-        }catch (error){
-            alert(error.message)
-        }
-    }
-
-    //withdraw
-    const withdraw = async () =>{
-        try {
-            const tx = await contract.withdraw()
-            await tx.wait()
-            alert(tx.hash)
-        }catch (error){
-            alert(error.message)
-        }
-
-    }
-
-    //gameEnded
-    const getGameStatus = async () =>{
-        try{
-            const status = await contract.gameEnded()
-            alert(status)
-        }catch (error){
-            alert(error.message)
-        }
-    }
-    //endGame
-    const endGame = async () =>{
-        try{
-            const tx =await contract.endGame()
-            await tx.wait()
-            alert(tx.hash)
-        }catch (error){
-            alert(error.message)
-        }
-    }
-    //restartGame
-    const restart = async () =>{
-        try{
-            //这里的5是测试数据
-            const tx =await contract.restartGame(5)
-            await tx.wait()
-            alert(tx.hash)
-        }catch (error){
-            alert(error.message)
-        }
-    }
-    const setTargetNumber = async ()=>{
-        try{
-            const tx =await  contract.setTargetNumber(5)
-            await tx.wait()
-            alert(tx.hash)
-        }catch (error){
-            alert(error.message)
-        }
-
-    }
-    function handleChainChanged(chainId) {
-        window.location.reload();
-    }
 
 
     return (
@@ -154,139 +133,167 @@ function Home() {
 
         <>
             <Head>
-                <link rel="shortcut icon" href="../static/cat.png" />
-                <title>2024-Dapp</title>
+                <link rel="shortcut icon" href="../static/img.png" />
+                <title>Web3 Prophet</title>
             </Head>
+            <Layout style={{ border: '1px solid var(--semi-color-border)' }}>
+                <Header style={{ backgroundColor: 'var(--semi-color-bg-1)' }}>
+                    <div>
+                        <Nav mode="horizontal" defaultSelectedKeys={['Home']}>
+                            <Nav.Header>
+                                <IconBeaker  style={{ fontSize: 36,color:"rgba(var(--semi-indigo-2), 1)" }} />
+                            </Nav.Header>
+                            <span
+                                style={{
+                                    color: 'var(--semi-color-text-2)',
+                                }}
+                            >
+                            <span
+                                style={{
+                                    marginRight: '24px',
+                                    color: 'rgba(var(--semi-light-green-5), 1)',
+                                    fontWeight: '600',
+                                }}
+                            >
+                               Web3 Prophet
+                            </span>
 
-            <div className="top">
-                <a href="/" style={{float:"left",color:"#2B333E",textDecoration:"none",fontSize:"28px",borderRadius:"8px"}}>Home</a>
-                <a href="vote" style={{marginLeft:"20px",color:"#2B333E",textDecoration:"none",fontSize:"28px",borderRadius:"8px"}}>Vote</a>
-                <a href="vote_test"style={{marginLeft:"20px",color:"#2B333E",textDecoration:"none",fontSize:"28px",borderRadius:"8px"}}>Test</a>
-                {account ?
-                    <button href="#" style={{ float: 'right' }} >🔐Connected</button>
-                    :
-                    <button href="#" style={{ float: 'right' }}onClick={connectOnclick} >🔓️Connect Wallet</button>
+                        </span>
+                            <Nav
+                                style={{ maxWidth: 220, height: '100%' }}
+                                items={[
+                                    { itemKey: 'Home', text: '首页',icon: <IconHome size="large" />,onClick: () => handleMenuClick('Home')},
+                                    { itemKey: 'Setting', text: '设置', icon: <IconSetting size="large" /> ,onClick: () => handleMenuClick('Setting')},
+                                ]}
+                            />
+
+                            <Nav.Footer>
+                                {switchStatus ?
+                                    <Tooltip
+                                        position='bottom'
+                                        content='切换到黑夜模式'>
+                                        <Button
+                                            theme="borderless"
+                                            onClick={switchMode}
+                                            icon={<IconSun />}
+                                            style={{
+                                                color: 'rgba(var(--semi-orange-4), 1)',
+                                                marginRight: '12px',
+                                            }}
+                                        />
+                                    </Tooltip>:
+                                    <Tooltip
+                                        position='bottom'
+                                        content='切换到白天模式'>
+                                        <Button
+                                            theme="borderless"
+                                            onClick={switchMode}
+                                            icon={<IconMoon />}
+                                            style={{
+                                                color: 'rgba(var(--semi-yellow-4), 1)',
+                                                marginRight: '12px',
+                                            }}
+                                        />
+                                    </Tooltip>
+                                }
+                                <Popover
+                                    content={
+                                        <Empty
+                                            title={'Web3Prophet Tips'}
+                                            description="1.为了安全，链上数据是keccak256哈希密文比对|
+                                    2.付一次钱可以猜三次|3.监听event来判断此时猜测状态"
+                                            style={{ width: 400, margin: '0 auto', display: 'flex', padding: 20 }}
+                                        />
+                                    }
+                                >
+                                    <Button
+                                        theme="borderless"
+                                        icon={<IconHelpCircle size="large" />}
+                                        style={{
+                                            color: 'rgba(var(--semi-indigo-1), 1)',
+                                            marginRight: '12px',
+                                        }}
+                                    />
+
+                                </Popover>
+                                {account ?
+                                    <>
+
+                                        <Button
+                                            href="#"
+                                            theme="borderless"
+                                            style={{  color: 'rgba(var(--semi-cyan-1), 1)', marginRight: '12px',}}
+                                            icon={<IconLock size="large"/>}
+                                        />
+                                        <Dropdown
+                                            render={
+                                                <Dropdown.Menu>
+                                                    <Dropdown.Item onClick={ToastAccount} icon={<IconGlobe /> } style={{color:"rgba(var(--semi-teal-8), 1)"}}>Account</Dropdown.Item>
+                                                    <Dropdown.Item onClick={ToastBalance} icon={<IconGallery />}style={{color:"rgba(var(--semi-yellow-6), 1)"}}>Balance</Dropdown.Item>
+                                                </Dropdown.Menu>
+                                            }
+                                        >
+                                            <Avatar
+                                                alt="beautiful cat"
+                                                src="../static/img.png"
+                                                style={{ margin: 4 }}
+                                            />
+                                        </Dropdown>
+                                    </>
+                                    :
+                                    <Tooltip
+                                        position='bottom'
+                                        content='点此按钮连接钱包'>
+                                        <Button
+                                            href="#"
+                                            theme="borderless"
+                                            style={{
+                                                color: 'rgba(var(--semi-cyan-1), 1)',
+                                                marginRight: '12px',}}
+                                            onClick={connectOnclick}
+                                            icon={<IconUnlock size="large"/>}
+
+                                        />
+                                    </Tooltip>
+
+                                }
+
+                            </Nav.Footer>
+                        </Nav>
+                    </div>
+                </Header>
+                {selectedKey === 'Home' && <ContractDeploy
+                    goToContractCall={goToContractCall}/>
                 }
-            </div>
-            <div className="container">
-
-                <h1 id="title" style={{textAlign: 'center', color: "deepskyblue"}}>🦄 Wallet Info</h1>
-                <h2 style={{color: "#8ABCD1"}}>Network Type:{chainId}</h2>
-                <h2 style={{color: "#1661AB"}}>Account:{account}</h2>
-                <h2 style={{color: "#1661AB"}}>Balance:{balance}</h2>
-
-                <hr/>
-
-                <h1 id="title" style={{textAlign: 'center', color: "#E9B6BE"}}>🌷 Call Contract</h1>
-                <input
-                    type="text"
-                    id="greet"
-                    value={number}
-                    onChange={(e) => setNumber(e.target.value)}
-                    placeholder="Input your guess Number"
-                />
-                <button onClick={guessNumber}>Guess the Number</button>
-                <button onClick={getOwner}>Get The Owner</button>
-                <button onClick={withdraw}>Withdraw</button>
-                <br/>
-                <br/>
-                <button onClick={getGameStatus}>GameEnded</button>
-                <button onClick={endGame}>endGame</button>
-                <br/>
-                <br/>
-                <button onClick={setTargetNumber}>setTargetNumber</button>
-                <br/>
-                <br/>
-                <button onClick={restart}>restart</button>
-                <h4 style={{textAlign: 'center', color: "grey"}}>The Greeting Contract Address is <a
-                    href="https://www.oklink.com/cn/sepolia-test/address/0x76f5c15b459044542169151f335575f501cf5ec0"
+                {selectedKey === 'ContractCall' && (
+                    <ContractCall
+                        contractAddress={contractAddress}
+                    />
+                )}
+                {selectedKey === 'Setting' && <Setting />}
+                <Footer
                     style={{
-                        textDecoration: "none",
-                        color: "lightslategrey"
-                    }}>0x76f5c15b459044542169151f335575f501cf5ec0</a></h4>
-                <h5 style={{color: "chocolate", textAlign: 'center'}}>Read the doc🦊<a
-                    href="https://docs.metamask.io/wallet/">https://docs.metamask.io/wallet/</a></h5>
-                <h5 style={{color: "slateblue", textAlign: 'center'}}>Ethers.js v6🍃<a
-                    href="https://docs.ethers.org/v6/">https://docs.ethers.org/v6/</a></h5>
-
-
-            </div>
-
-
-            <style jsx>
-            {`
-                    * {
-                        box-sizing: border-box;
-                    }
-
-                    body {
-                        font-family: Arial;
-                        padding: 10px;
-                        background: #f1f1f1;
-                    }
-
-
-                    .top {
-                        overflow: hidden;
-                    background-color: #E5DFD5;
-                    padding: 10px;
-                    margin-top: 0;
-                  }
-
-                  .top button {
-                    padding: 9px 16px;
-                    max-height: 40px;
-                    border-color: #a0b0ee;
-                    color: #eacd76;
-                    background-color: white;
-                    border-radius: 8px;
-                    align-items: center;
-                    font-size: 16px;
-                    text-align: center;
-                    font-weight: bold;
-                    cursor: pointer;
-                  }
-
-                  .container input {
-                    border-top-style: hidden;
-                    border-right-style: hidden;
-                    border-left-style: hidden;
-                    border-bottom-style: groove;
-                    font-size: 16px;
-                    width: 100%;
-                    border-color: rgba(4, 4, 5, 0.1);
-                    line-height: 32px;
-                  }
-
-                  .container button {
-                    padding: 9px 16px;
-                    max-height: 40px;
-                    border-color: #c8f8b8;
-                    color: #e7c8a1;
-                    background-color: #f1ebc5;
-                    border-radius: 8px;
-                    align-items: center;
-                    font-size: 16px;
-                    font-weight: 500;
-                    text-align: center;
-                    font-weight: bold;
-                    cursor: pointer;
-                  }
-                  
-                  .custom-alert {
-                    background-color: #f8d7da;
-                    color: #721c24;
-                    border: 1px solid #f5c6cb;
-                    padding: 10px;
-                    border-radius: 5px;
-                }
-
-
-
-
-                `}
-            </style>
-
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        padding: '20px',
+                        color: 'var(--semi-color-text-2)',
+                        backgroundColor: 'rgba(var(--semi-grey-0), 1)',
+                    }}
+                >
+                <span
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                    }}
+                >
+                    <IconBytedanceLogo size="large" style={{ marginRight: '8px' }} />
+                    <span>Copyright © 2024 Web3Prophet. All Rights Reserved. </span>
+                </span>
+                    <span>
+                    <span>反馈建议</span>
+                </span>
+                </Footer>
+            </Layout>
         </>
     )
 }
